2c4c380e1542ffc45607ce2e6bbb23373240f805
marver987 <marver987@gmail.com>
2020-11-04 21:52:49 -0400

    incluyendo login.php

---
+++ login.php
@@ -0,0 +1,37 @@
+<?php
+ // Iniciar la sesion y la concexion a la BD
+ require_once 'includes/conexion.php';
+
+ // Recoger datos del formulario
+if (isset($_POST)){
+  $Email = trim($_POST['nombresUsr']);
+  $contraseña = $_POST['passwordUsr'];
+
+  //Consulta para comprobar las credenciales del usuario
+  $sql = "SELECT * FROM usuario WHERE Email  = '$Email'";
+  $login = mysqli_query($db, $sql);
+
+  if($login && mysqli_num_rows($login) == 1){
+    $usuario = mysqli_fetch_assoc($login);
+    var_dump($usuario);
+    die();
+    
+    //Comprobar la contraseña / cifrar
+    $password_segura = password_hash($contraseña, PASSWORD_BCRYPT, ['cost'=> 4]);
+    var_dump($password_segura);
+    die();
+  }else{
+    //mensaje de error
+  }
+
+  
+}
+ 
+
+ 
+
+ //Utilizar una sesion para guardar los datos del usuario logueado
+
+ // Si algo falla enviar una sesion con el fallo 
+
+ //Redigir al index.php
